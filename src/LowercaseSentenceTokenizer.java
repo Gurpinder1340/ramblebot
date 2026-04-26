d98eb7177cfba50ba5e1865562d894a026075474
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-04-26 12:25:05 -0700

    finished lowercasesentence tokenizer

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A tokenizer that converts text input to lowercase and splits it 
@@ -29,8 +30,14 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    * @return a list of tokens, where each token is a word or a period
    */
   public List<String> tokenize(Scanner scanner) {
-    // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+      List<String> tokens = new ArrayList<>();
+
+      while (scanner.hasNext()) {
+        tokens.add(scanner.next());
+      }
+
+
+    return tokens;
   }
 }
 
